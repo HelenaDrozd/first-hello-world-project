7b0e5e0880c2c0319b5e63f6e471eb611f24eb4e
Helena Drozdowski <helenadrozd77@gmail.com>
2022-07-26 21:48:52 -0400

    Commiting in new_dev_branch

--- src/tek/simple/HelloWorld.java
+++ src/tek/simple/HelloWorld.java
@@ -3,6 +3,7 @@ package tek.simple;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		//Changes to the class in new_dev_branch git branch
 		System.out.println("Hello World!");
 
 	}
